88397069de3525021e1ac6107dc92b6d4b1a54eb
xdma5 <pgonzalez@accendgroup.com>
2017-11-21 12:44:58 -0800

    selenium test

--- src/main/java/com/companyname/myapp/App.java
+++ src/main/java/com/companyname/myapp/App.java
@@ -1,5 +1,8 @@
 package com.companyname.myapp;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
 /**
  * Hello world!
  *
@@ -8,7 +11,9 @@ public class App
 {
     public static void main( String[] args )
     {
+        WebDriver driver = new FirefoxDriver();
         System.out.println( "Hello World!" );
         System.out.println("hi");
+        driver.close();
     }
 }
