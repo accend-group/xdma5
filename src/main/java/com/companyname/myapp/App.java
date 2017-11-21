c243760b83a5fea4815e99adf93b91fa223bbf0d
xdma5 <pgonzalez@accendgroup.com>
2017-11-21 15:10:56 -0800

    selenium and pdfbox

--- src/main/java/com/companyname/myapp/App.java
+++ src/main/java/com/companyname/myapp/App.java
@@ -1,7 +1,15 @@
 package com.companyname.myapp;
 
+import com.companyname.myapp.pdf.PDFMaker;
+import com.companyname.myapp.selenium.Selenium;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.List;
+import java.util.Scanner;
 
 /**
  * Hello world!
@@ -9,12 +17,49 @@ import org.openqa.selenium.firefox.FirefoxDriver;
  */
 public class App
 {
+
+    private static String REMOVE_IMSAFETY_SCRIPT;
+    static{
+        try {
+            REMOVE_IMSAFETY_SCRIPT = new Scanner(new File("src/com/test/javascript/remove_imsafety.js")).useDelimiter("\\Z").next();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
     public static void main( String[] args )
     {
-        System.setProperty("webdriver.gecko.driver", "src/main/resources/geckodriver");
-        WebDriver driver = new FirefoxDriver();
-        System.out.println( "Hello World!" );
-        System.out.println("hi");
-        driver.quit();
+        PDFMaker pdfMaker = new PDFMaker();
+        Selenium localBrowser = new Selenium();
+        //localBrowser.setRemoteServer("http://0.0.0.0:4444/wd/hub");
+        localBrowser.start(false);
+
+        localBrowser.get("https://www.perjeta.com/patient/site-map.html");
+        //pdfMaker.addImg(localBrowser.getVisibleScreenshot());
+
+        List<String> links = null;
+        try {
+            links = (List<String>) localBrowser.runJS(new Scanner(new File("src/com/test/javascript/get_all_links.js")).useDelimiter("\\Z").next());
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        for (String page : links) {
+            localBrowser.get(page);
+            //localBrowser.runJS(" document.getElementById('mobile-uh-wrapper').style = 'position:absolute!important;';");
+
+            pdfMaker.addImg(localBrowser.getVisibleScreenshot());
+            localBrowser.runJS(REMOVE_IMSAFETY_SCRIPT);
+            pdfMaker.addImg(localBrowser.getFullPageScreenshot());
+        }
+
+        pdfMaker.createPDF();
+        pdfMaker.savePDF("perjetaDesktop.pdf");
+        try {
+            pdfMaker.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        localBrowser.stop();
     }
 }
---
+++ src/main/java/com/companyname/myapp/pdf/PDFMaker.java
@@ -0,0 +1,71 @@
+package com.companyname.myapp.pdf;
+
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.pdmodel.PDPage;
+import org.apache.pdfbox.pdmodel.PDPageContentStream;
+import org.apache.pdfbox.pdmodel.common.PDRectangle;
+import org.apache.pdfbox.pdmodel.graphics.image.LosslessFactory;
+import org.apache.pdfbox.pdmodel.graphics.image.PDImageXObject;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.LinkedList;
+
+public class PDFMaker {
+
+    private LinkedList<BufferedImage> images = new LinkedList<>();
+        private PDDocument pdf;
+
+    public void addImg(BufferedImage img){
+        images.add(img);
+    }
+
+    public void addImg(String imgFilePath) throws IOException{
+        images.add(ImageIO.read(new File(imgFilePath)));
+    }
+
+    public void addImg(File imgFile) throws IOException{
+        images.add(ImageIO.read(imgFile));
+    }
+
+    public void close() throws IOException{
+        if(pdf != null)
+            pdf.close();
+        images.clear();
+    }
+
+    // creates a pdf from the list of images
+    // https://stackoverflow.com/questions/22358478/java-create-pdf-pages-from-images-using-pdfbox-library
+    public void createPDF() {
+        try {
+            if (pdf != null)
+                pdf.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        pdf = new PDDocument();
+        try {
+            for (BufferedImage img : images) {
+                PDPage page = new PDPage(new PDRectangle(img.getWidth(), img.getHeight()));
+                pdf.addPage(page);
+                PDImageXObject pdImage = LosslessFactory.createFromImage(pdf, img);
+                PDPageContentStream pageContentStream = new PDPageContentStream(pdf, page);
+                pageContentStream.drawImage(pdImage, 0, 0);
+                pageContentStream.close();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void savePDF(String path){
+        try {
+            File newPdf = new File(path);
+            pdf.save(newPdf);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
