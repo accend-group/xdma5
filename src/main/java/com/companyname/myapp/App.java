1fc18e6c67e17809f3ce9b1468a8248729d8058f
xdma5 <pgonzalez@accendgroup.com>
2017-11-21 12:31:33 -0800

    new changes

---
+++ src/main/java/com/companyname/myapp/App.java
@@ -0,0 +1,14 @@
+package com.companyname.myapp;
+
+/**
+ * Hello world!
+ *
+ */
+public class App
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println("hi");
+    }
+}
