cf7d9bd9dbf2b2d5fdf75fa01198cbea2b21ee36
xdma5 <pgonzalez@accendgroup.com>
2017-11-30 15:33:53 -0800

    chromedriver headless screenshot test

---
+++ src/main/java/com/gene/screenshots/HeadlessTest.java
@@ -0,0 +1,48 @@
+package com.gene.screenshots;
+
+import com.assertthat.selenium_shutterbug.core.Shutterbug;
+import com.assertthat.selenium_shutterbug.utils.web.ScrollStrategy;
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.io.File;
+import java.io.IOException;
+
+
+public class HeadlessTest {
+    public static void main(String [] args){
+
+        System.setProperty("webdriver.chrome.driver", "chromedriver");
+
+        ChromeOptions options = new ChromeOptions();
+
+        //options.setBinary("/Applications/Google Chrome/Contents/MacOS/Google Chrome");
+        options.addArguments("--headless");
+
+
+        WebDriver chromeDriver = new ChromeDriver(options);
+        chromeDriver.manage().window().setSize(new Dimension(1600,1200));
+        chromeDriver.get("http://www.google.com");
+
+
+        String imageName = "google_headless_vis";
+        TakesScreenshot ts = (TakesScreenshot) chromeDriver;
+        File imageFile = ts.getScreenshotAs(OutputType.FILE);
+        try {
+            FileUtils.copyFile(imageFile, new File((imageName + ".png")));
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        imageName = "google_headless_full";
+        Shutterbug.shootPage(chromeDriver, ScrollStrategy.BOTH_DIRECTIONS, 350, true).withName(imageName).save(".");
+
+        chromeDriver.quit();
+    }
+}
