f78339f4bc77db83581fb4cab3d333445587877a
xdma5 <pgonzalez@accendgroup.com>
2017-12-01 11:19:46 -0800

    Generating pdf and using web drivers relative to node_modules directory

--- src/main/java/com/gene/screenshots/HeadlessTest.java
+++ src/main/java/com/gene/screenshots/HeadlessTest.java
@@ -2,6 +2,7 @@ package com.gene.screenshots;
 
 import com.assertthat.selenium_shutterbug.core.Shutterbug;
 import com.assertthat.selenium_shutterbug.utils.web.ScrollStrategy;
+import com.gene.screenshots.pdf.PDFMaker;
 import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.Dimension;
 import org.openqa.selenium.OutputType;
@@ -16,13 +17,17 @@ import org.openqa.selenium.firefox.FirefoxOptions;
 import java.io.File;
 import java.io.IOException;
 import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.Map;
 
 
 public class HeadlessTest {
     public static void main(String [] args){
 
-        System.setProperty("webdriver.chrome.driver", "chromedriver");
+        PDFMaker newPdf = new PDFMaker();
+        LinkedList<String> imageNames = new LinkedList<>();
+
+        System.setProperty("webdriver.chrome.driver", "node_modules/chromedriver/lib/chromedriver/chromedriver");
 
         ChromeOptions options = new ChromeOptions();
 
@@ -41,28 +46,29 @@ public class HeadlessTest {
 
         WebDriver chromeDriver = new ChromeDriver(options);
         //chromeDriver.manage().window().setSize(new Dimension(320,1200));
-        chromeDriver.get("http://www.google.com");
+        chromeDriver.get("http://www.perjeta.com");
 
 
-        String imageName = "google_headless_vis";
+        String imageName = "perjeta_headless_vis";
         TakesScreenshot ts = (TakesScreenshot) chromeDriver;
+
         File imageFile = ts.getScreenshotAs(OutputType.FILE);
         try {
-            FileUtils.copyFile(imageFile, new File((imageName + ".png")));
-
+            //FileUtils.copyFile(imageFile, new File((imageName + ".png")));
+            newPdf.addImg(imageFile);
         } catch (IOException e) {
             e.printStackTrace();
         }
 
-        imageName = "google_headless_full";
-        Shutterbug.shootPage(chromeDriver, ScrollStrategy.BOTH_DIRECTIONS, 350, true).withName(imageName).save(".");
+        imageName = "perjeta_headless_full";
+        newPdf.addImg(Shutterbug.shootPage(chromeDriver, ScrollStrategy.BOTH_DIRECTIONS, 350, true).getImage());//.withName(imageName).save(".");
 
         chromeDriver.quit();
 
 
-        System.setProperty("webdriver.gecko.driver", "geckodriver");
+        System.setProperty("webdriver.gecko.driver", "node_modules/geckodriver/geckodriver");
 
-        FirefoxBinary firefoxBinary = new FirefoxBinary(new File("~/home/anthony/firefox/firefox"));
+        FirefoxBinary firefoxBinary = new FirefoxBinary(new File("/home/anthony/firefox/firefox"));
         firefoxBinary.addCommandLineOptions("--headless");
         FirefoxOptions firefoxOptions = new FirefoxOptions();
         //firefoxOptions.setHeadless(true);
@@ -80,15 +86,23 @@ public class HeadlessTest {
         ts = (TakesScreenshot) geckoDriver;
         imageFile = ts.getScreenshotAs(OutputType.FILE);
         try {
-            FileUtils.copyFile(imageFile, new File((imageName + ".png")));
-
+            //FileUtils.copyFile(imageFile, new File((imageName + ".png")));
+            newPdf.addImg(imageFile);
         } catch (IOException e) {
             e.printStackTrace();
         }
 
         imageName = "firefox_headless_full";
-        Shutterbug.shootPage(geckoDriver, ScrollStrategy.BOTH_DIRECTIONS, 350, true).withName(imageName).save(".");
+        newPdf.addImg(Shutterbug.shootPage(geckoDriver, ScrollStrategy.BOTH_DIRECTIONS, 350, true).getImage());//.withName(imageName).save(".");
 
         geckoDriver.quit();
+
+
+        newPdf.savePDF("HeadlestTestPDF.pdf");
+        try {
+            newPdf.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
