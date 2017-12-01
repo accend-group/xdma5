de089276dca466e3b4d5e9a28eccd34987fdc5d2
xdma5 <pgonzalez@accendgroup.com>
2017-11-30 16:51:46 -0800

    Firefox headless cmdline argument screenshot test

--- src/main/java/com/gene/screenshots/HeadlessTest.java
+++ src/main/java/com/gene/screenshots/HeadlessTest.java
@@ -9,6 +9,9 @@ import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxBinary;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
 
 import java.io.File;
 import java.io.IOException;
@@ -44,5 +47,36 @@ public class HeadlessTest {
         Shutterbug.shootPage(chromeDriver, ScrollStrategy.BOTH_DIRECTIONS, 350, true).withName(imageName).save(".");
 
         chromeDriver.quit();
+
+
+        System.setProperty("webdriver.gecko.driver", "geckodriver");
+
+        FirefoxBinary firefoxBinary = new FirefoxBinary();
+        firefoxBinary.addCommandLineOptions("--headless");
+        FirefoxOptions firefoxOptions = new FirefoxOptions();
+        firefoxOptions.setBinary(firefoxBinary);
+        //options.setBinary("/Applications/Google Chrome/Contents/MacOS/Google Chrome");
+
+
+
+        WebDriver geckoDriver = new FirefoxDriver(firefoxOptions);
+        geckoDriver.manage().window().setSize(new Dimension(1600,1200));
+        geckoDriver.get("http://www.mozilla.org");
+
+
+        imageName = "firefox_headless_vis";
+        ts = (TakesScreenshot) geckoDriver;
+        imageFile = ts.getScreenshotAs(OutputType.FILE);
+        try {
+            FileUtils.copyFile(imageFile, new File((imageName + ".png")));
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        imageName = "firefox_headless_full";
+        Shutterbug.shootPage(geckoDriver, ScrollStrategy.BOTH_DIRECTIONS, 350, true).withName(imageName).save(".");
+
+        geckoDriver.quit();
     }
 }
