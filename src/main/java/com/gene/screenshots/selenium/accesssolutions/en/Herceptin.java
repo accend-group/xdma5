45e4a70df5b1a8abab443a1e752d6c68b1729581
Sharon Yee <syee@accendgroup.com>
2017-12-29 10:57:21 -0800

    Use accordion methods for Herceptin

--- src/main/java/com/gene/screenshots/selenium/accesssolutions/en/Herceptin.java
+++ src/main/java/com/gene/screenshots/selenium/accesssolutions/en/Herceptin.java
@@ -1,12 +1,10 @@
 package com.gene.screenshots.selenium.accesssolutions.en;
 
-import com.amazonaws.services.dynamodbv2.xspec.B;
 import com.gene.screenshots.Variables;
 import com.gene.screenshots.selenium.SeleniumHeadless;
 import org.openqa.selenium.*;
 import org.openqa.selenium.interactions.Actions;
 
-import static com.gene.screenshots.selenium.Constants.MOBILE_WIDTH;
 import static com.gene.screenshots.EnvironmentType.PROD;
 
 public class Herceptin extends SeleniumHeadless {
@@ -25,23 +23,11 @@ public class Herceptin extends SeleniumHeadless {
             full(driver, true, savePath, "accesssolutions-patient-herceptin-0.0");
 
             goToUrl(driver, "/patient/brands/herceptin/how-we-help-you.html");
-            movecursor(driver);
             Thread.sleep(1000);
             full(driver, true, savePath, "accesssolutions-patient-herceptin-1.0");
 
-            driver.findElement(By.xpath("//*[@id='accordion']/div/div[1]")).click();
-            movecursor(driver);
-            Thread.sleep(1000);
-            full(driver, true, savePath, "accesssolutions-patient-herceptin-1.0-tab1");
+            getScreenshotForAccordion(driver, "accesssolutions-patient-herceptin-1.0", savePath, true);
 
-            driver.findElement(By.xpath("//*[@id='accordion_0']/div/div[1]")).click();
-            movecursor(driver);
-            Thread.sleep(1000);
-            full(driver, true, savePath, "accesssolutions-patient-herceptin-1.0-tab2");
-            jse.executeScript("window.scrollTo(0, 0)");
-
-            jse.executeScript("window.scrollTo(0, 0)");
-            driver.findElement(By.xpath("//*[@id='accordion_0']/div/div[1]")).click();
             jse.executeScript("window.scrollTo(0, 0)");
             Thread.sleep(500);
             driver.findElement(By.xpath("//*[@id='accesssolutionsembed']/div/div/div/div[2]/div[1]/ul/li[1]/div[2]/fieldset/button[1]")).click();
@@ -100,69 +86,7 @@ public class Herceptin extends SeleniumHeadless {
             Thread.sleep(1000);
             full(driver, true, savePath, "accesssolutions-patient-herceptin-3.0");
 
-            jse.executeScript("window.scrollTo(0, 0)");
-            driver.findElement(By.xpath("//*[@id='accordion']/div/div[1]")).click();
-            movecursor(driver);
-            Thread.sleep(1000);
-            full(driver, true, savePath, "accesssolutions-patient-herceptin-3.0-tab1");
-
-            jse.executeScript("window.scrollTo(0, 0)");
-            driver.findElement(By.xpath("//*[@id='accordion_0']/div/div[1]")).click();
-            movecursor(driver);
-            Thread.sleep(1000);
-            full(driver, true, savePath, "accesssolutions-patient-herceptin-3.0-tab2");
-
-            jse.executeScript("window.scrollTo(0, 0)");
-            driver.findElement(By.xpath("//*[@id='accordion_1321572214']/div/div[1]")).click();
-            movecursor(driver);
-            Thread.sleep(1000);
-            full(driver, true, savePath, "accesssolutions-patient-herceptin-3.0-tab3");
-
-            jse.executeScript("window.scrollTo(0, 0)");
-            driver.findElement(By.xpath("//*[@id='accordion_2']/div[1]/div[1]")).click();
-            movecursor(driver);
-            Thread.sleep(1000);
-            full(driver, true, savePath, "accesssolutions-patient-herceptin-3.0-tab4");
-
-            jse.executeScript("window.scrollTo(0, 0)");
-            driver.findElement(By.xpath("//*[@id='accordion_2']/div[2]/div[1]")).click();
-            Thread.sleep(1000);
-            full(driver, true, savePath, "accesssolutions-patient-herceptin-3.0-tab5");
-
-            jse.executeScript("window.scrollTo(0, 0)");
-            driver.findElement(By.xpath("//*[@id='accordion_2']/div[3]/div[1]")).click();
-            movecursor(driver);
-            Thread.sleep(1000);
-            full(driver, true, savePath, "accesssolutions-patient-herceptin-3.0-tab6");
-
-            jse.executeScript("window.scrollTo(0, 0)");
-            driver.findElement(By.xpath("//*[@id='accordion_3']/div[1]/div[1]")).click();
-            movecursor(driver);
-            Thread.sleep(1000);
-            full(driver, true, savePath, "accesssolutions-patient-herceptin-3.0-tab7");
-
-            jse.executeScript("window.scrollTo(0, 0)");
-            driver.findElement(By.xpath("//*[@id='accordion_3']/div[2]/div[1]")).click();
-            movecursor1(driver);
-            Thread.sleep(1000);
-            full(driver, true, savePath, "accesssolutions-patient-herceptin-3.0-tab8");
-
-            driver.findElement(By.xpath("//*[@id='accordion_3']/div[3]/div[1]")).click();
-            movecursor1(driver);
-            Thread.sleep(1000);
-            full(driver, true, savePath, "accesssolutions-patient-herceptin-3.0-tab9");
-
-            jse.executeScript("window.scrollTo(0, 0)");
-            driver.findElement(By.xpath("//*[@id='accordion_4']/div[1]/div[1]")).click();
-            movecursor1(driver);
-            Thread.sleep(1000);
-            full(driver, true, savePath, "accesssolutions-patient-herceptin-3.0-tab10");
-
-            jse.executeScript("window.scrollTo(0, 0)");
-            driver.findElement(By.xpath("//*[@id='accordion_4']/div[2]/div[1]")).click();
-            movecursor(driver);
-            Thread.sleep(1000);
-            full(driver, true, savePath, "accesssolutions-patient-herceptin-3.0-tab11");
+            getScreenshotForAccordion(driver, "accesssolutions-patient-herceptin-3.0", savePath, true);
 
             goToUrl(driver, "/patient/brands/herceptin/patient-assistance-tool-page.html");
             Thread.sleep(1000);
@@ -250,22 +174,11 @@ public class Herceptin extends SeleniumHeadless {
             full(driver, false, savePath, "accesssolutions-mobile-patient-herceptin-0.0");
 
             goToUrl(driver, "/patient/brands/herceptin/how-we-help-you.html");
-            movecursorm(driver);
             Thread.sleep(1000);
             full(driver, false, savePath, "accesssolutions-mobile-patient-herceptin-1.0");
 
-            driver.findElement(By.xpath("//*[@id='accordion']/div/div[1]")).click();
-            movecursorm(driver);
-            Thread.sleep(1000);
-            full(driver, false, savePath, "accesssolutions-mobile-patient-herceptin-1.0-tab1");
+            getScreenshotForAccordion(driver, "accesssolutions-mobile-patient-herceptin-1.0", savePath, false);
 
-            driver.findElement(By.xpath("//*[@id='accordion_0']/div/div[1]")).click();
-            movecursorm(driver);
-            Thread.sleep(1000);
-            full(driver, false, savePath, "accesssolutions-mobile-patient-herceptin-1.0-tab2");
-
-            jse.executeScript("window.scrollTo(0, 0)");
-            driver.findElement(By.xpath("//*[@id='accordion_0']/div/div[1]")).click();
             jse.executeScript("window.scrollTo(0, 0)");
             Thread.sleep(500);
             driver.findElement(By.xpath("//*[@id='accesssolutionsembed']/div/div/div/div[2]/div[1]/ul/li[1]/div[2]/fieldset/button[1]")).click();
@@ -349,69 +262,7 @@ public class Herceptin extends SeleniumHeadless {
             Thread.sleep(1000);
             full(driver, false, savePath, "accesssolutions-mobile-patient-herceptin-3.0");
 
-            jse.executeScript("window.scrollTo(0, 0)");
-            driver.findElement(By.xpath("//*[@id='accordion']/div/div[1]")).click();
-            movecursorm(driver);
-            Thread.sleep(1000);
-            full(driver, false, savePath, "accesssolutions-mobile-patient-herceptin-3.0-tab1");
-
-            jse.executeScript("window.scrollTo(0, 0)");
-            driver.findElement(By.xpath("//*[@id='accordion_0']/div/div[1]")).click();
-            movecursorm(driver);
-            Thread.sleep(1000);
-            full(driver, false, savePath, "accesssolutions-mobile-patient-herceptin-3.0-tab2");
-
-            jse.executeScript("window.scrollTo(0, 0)");
-            driver.findElement(By.xpath("//*[@id='accordion_1321572214']/div/div[1]")).click();
-            movecursorm(driver);
-            Thread.sleep(1000);
-            full(driver, false, savePath, "accesssolutions-mobile-patient-herceptin-3.0-tab3");
-
-            jse.executeScript("window.scrollTo(0, 0)");
-            driver.findElement(By.xpath("//*[@id='accordion_2']/div[1]/div[1]")).click();
-            movecursorm(driver);
-            Thread.sleep(1000);
-            full(driver, false, savePath, "accesssolutions-mobile-patient-herceptin-3.0-tab4");
-
-            jse.executeScript("window.scrollTo(0, 0)");
-            driver.findElement(By.xpath("//*[@id='accordion_2']/div[2]/div[1]")).click();
-            Thread.sleep(1000);
-            full(driver, false, savePath, "accesssolutions-mobile-patient-herceptin-3.0-tab5");
-
-            jse.executeScript("window.scrollTo(0, 0)");
-            driver.findElement(By.xpath("//*[@id='accordion_2']/div[3]/div[1]")).click();
-            movecursorm(driver);
-            Thread.sleep(1000);
-            full(driver, false, savePath, "accesssolutions-mobile-patient-herceptin-3.0-tab6");
-
-            jse.executeScript("window.scrollTo(0, 0)");
-            driver.findElement(By.xpath("//*[@id='accordion_3']/div[1]/div[1]")).click();
-            movecursorm(driver);
-            Thread.sleep(1000);
-            full(driver, false, savePath, "accesssolutions-mobile-patient-herceptin-3.0-tab7");
-
-            jse.executeScript("window.scrollTo(0, 0)");
-            driver.findElement(By.xpath("//*[@id='accordion_3']/div[2]/div[1]")).click();
-            movecursorm(driver);
-            Thread.sleep(1000);
-            full(driver, false, savePath, "accesssolutions-mobile-patient-herceptin-3.0-tab8");
-
-            driver.findElement(By.xpath("//*[@id='accordion_3']/div[3]/div[1]")).click();
-            movecursorm(driver);
-            Thread.sleep(1000);
-            full(driver, false, savePath, "accesssolutions-mobile-patient-herceptin-3.0-tab9");
-
-            jse.executeScript("window.scrollTo(0, 0)");
-            driver.findElement(By.xpath("//*[@id='accordion_4']/div[1]/div[1]")).click();
-            movecursorm(driver);
-            Thread.sleep(1000);
-            full(driver, false, savePath, "accesssolutions-mobile-patient-herceptin-3.0-tab10");
-
-            jse.executeScript("window.scrollTo(0, 0)");
-            driver.findElement(By.xpath("//*[@id='accordion_4']/div[2]/div[1]")).click();
-            movecursorm(driver);
-            Thread.sleep(1000);
-            full(driver, false, savePath, "accesssolutions-mobile-patient-herceptin-3.0-tab11");
+            getScreenshotForAccordion(driver, "accesssolutions-mobile-patient-herceptin-3.0", savePath, false);
 
             goToUrl(driver, "/patient/brands/herceptin/patient-assistance-tool-page.html");
             Thread.sleep(1000);
@@ -479,28 +330,6 @@ public class Herceptin extends SeleniumHeadless {
             driver.quit();
         }
 
-    }
-
-    public static void movecursor(WebDriver driver) {
-
-        JavascriptExecutor jse = (JavascriptExecutor) driver;
-        Actions action = new Actions(driver);
-        WebElement element;
-
-        jse.executeScript("window.scrollTo(0, 0)");
-        element = driver.findElement(By.name("q"));
-        action.moveToElement(element).build().perform();
-    }
-
-    public static void movecursor1(WebDriver driver) {
-
-        JavascriptExecutor jse = (JavascriptExecutor) driver;
-        Actions action = new Actions(driver);
-        WebElement element;
-
-        jse.executeScript("window.scrollTo(0, 0)");
-        element = driver.findElement(By.linkText("FAQs"));
-        action.moveToElement(element).build().perform();
     }
 
     public static void movecursorm(WebDriver driver) {
