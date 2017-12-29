1a598c6bce4aaffc8db94a76ff3d885562be6b9a
Sharon Yee <syee@accendgroup.com>
2017-12-28 17:35:07 -0800

    Use accordion methods for Erivedge

--- src/main/java/com/gene/screenshots/selenium/accesssolutions/es/Erivedge.java
+++ src/main/java/com/gene/screenshots/selenium/accesssolutions/es/Erivedge.java
@@ -27,13 +27,8 @@ public class Erivedge extends SeleniumHeadless {
             Thread.sleep(1000);
             full(driver, true, savePath, "accesssolutions-patient-erivedge-1.0");
 
-            driver.findElement(By.xpath("//*[@id='accordion']/div/div[1]")).click();
-            movecursor(driver);
-            Thread.sleep(1000);
-            full(driver, true, savePath, "accesssolutions-patient-erivedge-1.0-tab1");
+            getScreenshotForAccordion(driver, "accesssolutions-patient-erivedge-1.0", savePath, true);
 
-            jse.executeScript("window.scrollTo(0, 0)");
-            driver.findElement(By.xpath("//*[@id='accordion']/div/div[1]")).click();
             jse.executeScript("window.scrollTo(0, 0)");
             Thread.sleep(500);
             driver.findElement(By.xpath("//*[@id='accesssolutionsembed']/div/div/div/div[2]/div[1]/ul/li[1]/div[2]/fieldset/button[1]")).click();
@@ -88,67 +83,7 @@ public class Erivedge extends SeleniumHeadless {
             Thread.sleep(1000);
             full(driver, true, savePath, "accesssolutions-patient-erivedge-3.0");
 
-            driver.findElement(By.xpath("//*[@id='accordion']/div/div[1]")).click();
-            movecursor(driver);
-            Thread.sleep(1000);
-            full(driver, true, savePath, "accesssolutions-patient-erivedge-3.0-tab1");
-
-            driver.findElement(By.xpath("//*[@id='accordion_0']/div/div[1]")).click();
-            movecursor(driver);
-            driver.manage().window().setSize(new Dimension(1621, 780));
-            Thread.sleep(1000);
-            full(driver, true, savePath, "accesssolutions-patient-erivedge-3.0-tab2");
-
-            driver.manage().window().maximize();
-            driver.findElement(By.xpath("//*[@id='accordion_1018444656']/div[1]/div[1]")).click();
-            movecursor(driver);
-            Thread.sleep(1000);
-            full(driver, true, savePath, "accesssolutions-patient-erivedge-3.0-tab3");
-
-            driver.findElement(By.xpath("//*[@id='accordion_1018444656']/div[2]/div[1]")).click();
-            movecursor(driver);
-            Thread.sleep(1000);
-            full(driver, true, savePath, "accesssolutions-patient-erivedge-3.0-tab4");
-
-            driver.findElement(By.xpath("//*[@id='accordion_2']/div[1]/div[1]")).click();
-            movecursor(driver);
-            Thread.sleep(1000);
-            full(driver, true, savePath, "accesssolutions-patient-erivedge-3.0-tab5");
-
-            driver.findElement(By.xpath("//*[@id='accordion_2']/div[2]/div[1]")).click();
-            movecursor(driver);
-            Thread.sleep(1000);
-            full(driver, true, savePath, "accesssolutions-patient-erivedge-3.0-tab6");
-
-            driver.findElement(By.xpath("//*[@id='accordion_2']/div[3]/div[1]")).click();
-            movecursor(driver);
-            Thread.sleep(1000);
-            full(driver, true, savePath, "accesssolutions-patient-erivedge-3.0-tab7");
-
-            driver.findElement(By.xpath("//*[@id='accordion_3']/div[1]/div[1]")).click();
-            movecursor(driver);
-            Thread.sleep(1000);
-            full(driver, true, savePath, "accesssolutions-patient-erivedge-3.0-tab8");
-
-            driver.findElement(By.xpath("//*[@id='accordion_3']/div[2]/div[1]")).click();
-            movecursor1(driver);
-            Thread.sleep(1000);
-            full(driver, true, savePath, "accesssolutions-patient-erivedge-3.0-tab9");
-
-            driver.findElement(By.xpath("//*[@id='accordion_3']/div[3]/div[1]")).click();
-            movecursor1(driver);
-            Thread.sleep(1000);
-            full(driver, true, savePath, "accesssolutions-patient-erivedge-3.0-tab10");
-
-            driver.findElement(By.xpath("//*[@id='accordion_4']/div[1]/div[1]")).click();
-            movecursor1(driver);
-            Thread.sleep(1000);
-            full(driver, true, savePath, "accesssolutions-patient-erivedge-3.0-tab11");
-
-            driver.findElement(By.xpath("//*[@id='accordion_4']/div[2]/div[1]")).click();
-            movecursor(driver);
-            Thread.sleep(1000);
-            full(driver, true, savePath, "accesssolutions-patient-erivedge-3.0-tab12");
+            getScreenshotForAccordion(driver, "accesssolutions-patient-erivedge-3.0", savePath, true);
 
             goToUrl(driver, "/patient/brands/erivedge/patient-assistance-tool-page.html");
             Thread.sleep(1000);
@@ -243,11 +178,7 @@ public class Erivedge extends SeleniumHeadless {
             Thread.sleep(1000);
             full(driver, false, savePath, "accesssolutions-mobile-patient-erivedge-1.0");
 
-            driver.findElement(By.xpath("//*[@id='accordion']/div/div[1]")).click();
-            movecursorm(driver);
-            Thread.sleep(1000);
-            full(driver, false, savePath, "accesssolutions-mobile-patient-erivedge-1.0-tab1");
-            driver.findElement(By.xpath("//*[@id='accordion']/div/div[1]")).click();
+            getScreenshotForAccordion(driver, "accesssolutions-mobile-patient-erivedge-1.0", savePath, false);
 
             jse.executeScript("window.scrollTo(0, 0)");
             Thread.sleep(500);
@@ -306,65 +237,7 @@ public class Erivedge extends SeleniumHeadless {
             Thread.sleep(1000);
             full(driver, false, savePath, "accesssolutions-mobile-patient-erivedge-3.0");
 
-            driver.findElement(By.xpath("//*[@id='accordion']/div/div[1]")).click();
-            movecursorm(driver);
-            Thread.sleep(1000);
-            full(driver, false, savePath, "accesssolutions-mobile-patient-erivedge-3.0-tab1");
-
-            driver.findElement(By.xpath("//*[@id='accordion_0']/div/div[1]")).click();
-            movecursorm(driver);
-            Thread.sleep(1000);
-            full(driver, false, savePath, "accesssolutions-mobile-patient-erivedge-3.0-tab2");
-
-            driver.findElement(By.xpath("//*[@id='accordion_1018444656']/div[1]/div[1]")).click();
-            movecursorm(driver);
-            Thread.sleep(1000);
-            full(driver, false, savePath, "accesssolutions-mobile-patient-erivedge-3.0-tab3");
-
-            driver.findElement(By.xpath("//*[@id='accordion_1018444656']/div[2]/div[1]")).click();
-            movecursorm(driver);
-            Thread.sleep(1000);
-            full(driver, false, savePath, "accesssolutions-mobile-patient-erivedge-3.0-tab4");
-
-            driver.findElement(By.xpath("//*[@id='accordion_2']/div[1]/div[1]")).click();
-            movecursorm(driver);
-            Thread.sleep(1000);
-            full(driver, false, savePath, "accesssolutions-mobile-patient-erivedge-3.0-tab5");
-
-            driver.findElement(By.xpath("//*[@id='accordion_2']/div[2]/div[1]")).click();
-            movecursorm(driver);
-            Thread.sleep(1000);
-            full(driver, false, savePath, "accesssolutions-mobile-patient-erivedge-3.0-tab6");
-
-            driver.findElement(By.xpath("//*[@id='accordion_2']/div[3]/div[1]")).click();
-            movecursorm(driver);
-            Thread.sleep(1000);
-            full(driver, false, savePath, "accesssolutions-mobile-patient-erivedge-3.0-tab7");
-
-            driver.findElement(By.xpath("//*[@id='accordion_3']/div[1]/div[1]")).click();
-            movecursorm(driver);
-            Thread.sleep(1000);
-            full(driver, false, savePath, "accesssolutions-mobile-patient-erivedge-3.0-tab8");
-
-            driver.findElement(By.xpath("//*[@id='accordion_3']/div[2]/div[1]")).click();
-            movecursorm(driver);
-            Thread.sleep(1000);
-            full(driver, false, savePath, "accesssolutions-mobile-patient-erivedge-3.0-tab9");
-
-            driver.findElement(By.xpath("//*[@id='accordion_3']/div[3]/div[1]")).click();
-            movecursorm(driver);
-            Thread.sleep(1000);
-            full(driver, false, savePath, "accesssolutions-mobile-patient-erivedge-3.0-tab10");
-
-            driver.findElement(By.xpath("//*[@id='accordion_4']/div[1]/div[1]")).click();
-            movecursorm(driver);
-            Thread.sleep(1000);
-            full(driver, false, savePath, "accesssolutions-mobile-patient-erivedge-3.0-tab11");
-
-            driver.findElement(By.xpath("//*[@id='accordion_4']/div[2]/div[1]")).click();
-            movecursorm(driver);
-            Thread.sleep(1000);
-            full(driver, false, savePath, "accesssolutions-mobile-patient-erivedge-3.0-tab12");
+            getScreenshotForAccordion(driver, "accesssolutions-mobile-patient-erivedge-3.0", savePath, false);
 
             goToUrl(driver, "/patient/brands/erivedge/patient-assistance-tool-page.html");
             Thread.sleep(1000);
@@ -415,28 +288,6 @@ public class Erivedge extends SeleniumHeadless {
             driver.close();
             driver.quit();
         }
-    }
-
-    public static void movecursor(WebDriver driver) {
-
-        JavascriptExecutor jse = (JavascriptExecutor) driver;
-        Actions action = new Actions(driver);
-        WebElement element;
-
-        jse.executeScript("window.scrollTo(0, 0)");
-        element = driver.findElement(By.name("q"));
-        action.moveToElement(element).build().perform();
-    }
-
-    public static void movecursor1(WebDriver driver) {
-
-        JavascriptExecutor jse = (JavascriptExecutor) driver;
-        Actions action = new Actions(driver);
-        WebElement element;
-
-        jse.executeScript("window.scrollTo(0, 0)");
-        element = driver.findElement(By.linkText("Preguntas Frecuentes"));
-        action.moveToElement(element).build().perform();
     }
 
     public static void movecursorm(WebDriver driver) {
