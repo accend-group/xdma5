3169d61f16b1c31dd886f5005ec42ff0dcc9e8f8
Sharon Yee <syee@accendgroup.com>
2017-12-29 10:25:42 -0800

    Use accordion method for hemlibra

--- src/main/java/com/gene/screenshots/selenium/accesssolutions/en/Hemlibra.java
+++ src/main/java/com/gene/screenshots/selenium/accesssolutions/en/Hemlibra.java
@@ -84,68 +84,7 @@ public class Hemlibra extends SeleniumHeadless {
             Thread.sleep(1000);
             full(driver, true, savePath, "accesssolutions-patient-hemlibra-3.0");
 
-            driver.manage().window().setSize(new Dimension(1621, 720));
-            driver.findElement(By.xpath("//*[@id='accordion']/div/div[1]")).click();
-            movecursor(driver);
-            Thread.sleep(1000);
-            full(driver, true, savePath, "accesssolutions-patient-hemlibra-3.0-tab1");
-
-            driver.findElement(By.xpath("//*[@id='accordion_0']/div/div[1]")).click();
-            Thread.sleep(1000);
-            full(driver, true, savePath, "accesssolutions-patient-hemlibra-3.0-tab2");
-
-            driver.manage().window().setSize(new Dimension(1621, 710));
-            driver.findElement(By.xpath("//*[@id='accordion_295986680']/div[1]/div[1]")).click();
-            movecursor(driver);
-            Thread.sleep(1000);
-            full(driver, true, savePath, "accesssolutions-patient-hemlibra-3.0-tab3");
-
-            driver.findElement(By.xpath("//*[@id='accordion_295986680']/div[2]/div[1]")).click();
-            movecursor3(driver);
-            Thread.sleep(1000);
-            full(driver, true, savePath, "accesssolutions-patient-hemlibra-3.0-tab4");
-
-            driver.manage().window().setSize(new Dimension(1621, 710));
-            driver.findElement(By.xpath("//*[@id='accordion_2']/div[1]/div[1]")).click();
-            movecursor(driver);
-            Thread.sleep(1000);
-            full(driver, true, savePath, "accesssolutions-patient-hemlibra-3.0-tab5");
-
-            driver.findElement(By.xpath("//*[@id='accordion_2']/div[2]/div[1]")).click();
-            movecursor(driver);
-            Thread.sleep(1000);
-            full(driver, true, savePath, "accesssolutions-patient-hemlibra-3.0-tab6");
-
-            driver.findElement(By.xpath("//*[@id='accordion_2']/div[3]/div[1]")).click();
-            movecursor(driver);
-            Thread.sleep(1000);
-            full(driver, true, savePath, "accesssolutions-patient-hemlibra-3.0-tab7");
-
-            driver.manage().window().maximize();
-            driver.findElement(By.xpath("//*[@id='accordion_3']/div[1]/div[1]")).click();
-            movecursor(driver);
-            Thread.sleep(1000);
-            full(driver, true, savePath, "accesssolutions-patient-hemlibra-3.0-tab8");
-
-            driver.findElement(By.xpath("//*[@id='accordion_3']/div[2]/div[1]")).click();
-            movecursor(driver);
-            Thread.sleep(1000);
-            full(driver, true, savePath, "accesssolutions-patient-hemlibra-3.0-tab9");
-
-            driver.findElement(By.xpath("//*[@id='accordion_3']/div[3]/div[1]")).click();
-            movecursor1(driver);
-            Thread.sleep(1000);
-            full(driver, true, savePath, "accesssolutions-patient-hemlibra-3.0-tab10");
-
-            driver.findElement(By.xpath("//*[@id='accordion_4']/div[1]/div[1]")).click();
-            movecursor(driver);
-            Thread.sleep(1000);
-            full(driver, true, savePath, "accesssolutions-patient-hemlibra-3.0-tab11");
-
-            driver.findElement(By.xpath("//*[@id='accordion_4']/div[2]/div[1]")).click();
-            movecursor(driver);
-            Thread.sleep(1000);
-            full(driver, true, savePath, "accesssolutions-patient-hemlibra-3.0-tab12");
+            getScreenshotForAccordion(driver, "accesssolutions-patient-hemlibra-3.0", savePath, true);
 
             goToUrl(driver, "/patient/brands/hemlibra/patient-assistance-tool-page.html");
             Thread.sleep(1000);
@@ -328,65 +267,7 @@ public class Hemlibra extends SeleniumHeadless {
             Thread.sleep(1000);
             full(driver, false, savePath, "accesssolutions-mobile-patient-hemlibra-3.0");
 
-            driver.findElement(By.xpath("//*[@id='accordion']/div/div[1]")).click();
-            movecursorm(driver);
-            Thread.sleep(1000);
-            full(driver, false, savePath, "accesssolutions-mobile-patient-hemlibra-3.0-tab1");
-
-            driver.findElement(By.xpath("//*[@id='accordion_0']/div/div[1]")).click();
-            movecursorm(driver);
-            Thread.sleep(1000);
-            full(driver, false, savePath, "accesssolutions-mobile-patient-hemlibra-3.0-tab2");
-
-            driver.findElement(By.xpath("//*[@id='accordion_295986680']/div[1]/div[1]")).click();
-            movecursorm(driver);
-            Thread.sleep(1000);
-            full(driver, false, savePath, "accesssolutions-mobile-patient-hemlibra-3.0-tab3");
-
-            driver.findElement(By.xpath("//*[@id='accordion_295986680']/div[2]/div[1]")).click();
-            movecursorm(driver);
-            Thread.sleep(1000);
-            full(driver, false, savePath, "accesssolutions-mobile-patient-hemlibra-3.0-tab4");
-
-            driver.findElement(By.xpath("//*[@id='accordion_2']/div[1]/div[1]")).click();
-            movecursorm(driver);
-            Thread.sleep(1000);
-            full(driver, false, savePath, "accesssolutions-mobile-patient-hemlibra-3.0-tab5");
-
-            driver.findElement(By.xpath("//*[@id='accordion_2']/div[2]/div[1]")).click();
-            movecursorm(driver);
-            Thread.sleep(1000);
-            full(driver, false, savePath, "accesssolutions-mobile-patient-hemlibra-3.0-tab6");
-
-            driver.findElement(By.xpath("//*[@id='accordion_2']/div[3]/div[1]")).click();
-            movecursorm(driver);
-            Thread.sleep(1000);
-            full(driver, false, savePath, "accesssolutions-mobile-patient-hemlibra-3.0-tab7");
-
-            driver.findElement(By.xpath("//*[@id='accordion_3']/div[1]/div[1]")).click();
-            movecursorm(driver);
-            Thread.sleep(1000);
-            full(driver, false, savePath, "accesssolutions-mobile-patient-hemlibra-3.0-tab8");
-
-            driver.findElement(By.xpath("//*[@id='accordion_3']/div[2]/div[1]")).click();
-            movecursorm(driver);
-            Thread.sleep(1000);
-            full(driver, false, savePath, "accesssolutions-mobile-patient-hemlibra-3.0-tab9");
-
-            driver.findElement(By.xpath("//*[@id='accordion_3']/div[3]/div[1]")).click();
-            movecursorm(driver);
-            Thread.sleep(1000);
-            full(driver, false, savePath, "accesssolutions-mobile-patient-hemlibra-3.0-tab10");
-
-            driver.findElement(By.xpath("//*[@id='accordion_4']/div[1]/div[1]")).click();
-            movecursorm(driver);
-            Thread.sleep(1000);
-            full(driver, false, savePath, "accesssolutions-mobile-patient-hemlibra-3.0-tab11");
-
-            driver.findElement(By.xpath("//*[@id='accordion_4']/div[2]/div[1]")).click();
-            movecursorm(driver);
-            Thread.sleep(1000);
-            full(driver, false, savePath, "accesssolutions-mobile-patient-hemlibra-3.0-tab12");
+            getScreenshotForAccordion(driver, "accesssolutions-mobile-patient-hemlibra-3.0", savePath, false);
 
             goToUrl(driver, "/patient/brands/hemlibra/patient-assistance-tool-page.html");
             Thread.sleep(1000);
@@ -466,17 +347,6 @@ public class Hemlibra extends SeleniumHeadless {
         action.moveToElement(element).build().perform();
     }
 
-    public static void movecursor1(WebDriver driver) {
-
-        JavascriptExecutor jse = (JavascriptExecutor) driver;
-        Actions action = new Actions(driver);
-        WebElement element;
-
-        jse.executeScript("window.scrollTo(0, 0)");
-        element = driver.findElement(By.linkText("FAQs"));
-        action.moveToElement(element).build().perform();
-    }
-
     public static void movecursorm(WebDriver driver) {
 
         JavascriptExecutor jse = (JavascriptExecutor) driver;
@@ -485,17 +355,6 @@ public class Hemlibra extends SeleniumHeadless {
 
         jse.executeScript("window.scrollTo(0, 0)");
         element = driver.findElement(By.xpath("/html/body/div[2]/div[2]/div/div/div[2]/div[1]/div[2]/button"));
-        action.moveToElement(element).build().perform();
-    }
-
-    public static void movecursor3(WebDriver driver) {
-
-        JavascriptExecutor jse = (JavascriptExecutor) driver;
-        Actions action = new Actions(driver);
-        WebElement element;
-
-        jse.executeScript("window.scrollTo(0, 0)");
-        element = driver.findElement(By.xpath("//*[@id='logo']/a/div/img"));
         action.moveToElement(element).build().perform();
     }
 }
--- src/main/java/com/gene/screenshots/selenium/accesssolutions/es/Hemlibra.java
+++ src/main/java/com/gene/screenshots/selenium/accesssolutions/es/Hemlibra.java
@@ -76,68 +76,7 @@ public class Hemlibra extends SeleniumHeadless {
             Thread.sleep(1000);
             full(driver, true, savePath, "accesssolutions-patient-hemlibra-3.0");
 
-            driver.manage().window().setSize(new Dimension(1621, 720));
-            driver.findElement(By.xpath("//*[@id='accordion']/div/div[1]")).click();
-            movecursor(driver);
-            Thread.sleep(1000);
-            full(driver, true, savePath, "accesssolutions-patient-hemlibra-3.0-tab1");
-
-            driver.findElement(By.xpath("//*[@id='accordion_0']/div/div[1]")).click();
-            Thread.sleep(1000);
-            full(driver, true, savePath, "accesssolutions-patient-hemlibra-3.0-tab2");
-
-            driver.manage().window().setSize(new Dimension(1621, 710));
-            driver.findElement(By.xpath("//*[@id='accordion_295986680']/div[1]/div[1]")).click();
-            movecursor(driver);
-            Thread.sleep(1000);
-            full(driver, true, savePath, "accesssolutions-patient-hemlibra-3.0-tab3");
-
-            driver.findElement(By.xpath("//*[@id='accordion_295986680']/div[2]/div[1]")).click();
-            movecursor(driver);
-            driver.manage().window().setSize(new Dimension(1621, 710));
-            Thread.sleep(1000);
-            full(driver, true, savePath, "accesssolutions-patient-hemlibra-3.0-tab4");
-
-            driver.findElement(By.xpath("//*[@id='accordion_2']/div[1]/div[1]")).click();
-            movecursor(driver);
-            Thread.sleep(1000);
-            full(driver, true, savePath, "accesssolutions-patient-hemlibra-3.0-tab5");
-
-            driver.findElement(By.xpath("//*[@id='accordion_2']/div[2]/div[1]")).click();
-            movecursor(driver);
-            Thread.sleep(1000);
-            full(driver, true, savePath, "accesssolutions-patient-hemlibra-3.0-tab6");
-
-            driver.findElement(By.xpath("//*[@id='accordion_2']/div[3]/div[1]")).click();
-            movecursor(driver);
-            Thread.sleep(1000);
-            full(driver, true, savePath, "accesssolutions-patient-hemlibra-3.0-tab7");
-
-            driver.manage().window().maximize();
-            driver.findElement(By.xpath("//*[@id='accordion_3']/div[1]/div[1]")).click();
-            movecursor(driver);
-            Thread.sleep(1000);
-            full(driver, true, savePath, "accesssolutions-patient-hemlibra-3.0-tab8");
-
-            driver.findElement(By.xpath("//*[@id='accordion_3']/div[2]/div[1]")).click();
-            movecursor(driver);
-            Thread.sleep(1000);
-            full(driver, true, savePath, "accesssolutions-patient-hemlibra-3.0-tab9");
-
-            driver.findElement(By.xpath("//*[@id='accordion_3']/div[3]/div[1]")).click();
-            movecursor1(driver);
-            Thread.sleep(1000);
-            full(driver, true, savePath, "accesssolutions-patient-hemlibra-3.0-tab10");
-
-            driver.findElement(By.xpath("//*[@id='accordion_4']/div[1]/div[1]")).click();
-            movecursor(driver);
-            Thread.sleep(1000);
-            full(driver, true, savePath, "accesssolutions-patient-hemlibra-3.0-tab11");
-
-            driver.findElement(By.xpath("//*[@id='accordion_4']/div[2]/div[1]")).click();
-            movecursor(driver);
-            Thread.sleep(1000);
-            full(driver, true, savePath, "accesssolutions-patient-hemlibra-3.0-tab12");
+            getScreenshotForAccordion(driver, "accesssolutions-patient-hemlibra-3.0", savePath, true);
 
             goToUrl(driver, "/patient/brands/hemlibra/patient-assistance-tool-page.html");
             Thread.sleep(1000);
@@ -282,65 +221,7 @@ public class Hemlibra extends SeleniumHeadless {
             Thread.sleep(1000);
             full(driver, false, savePath, "accesssolutions-mobile-patient-hemlibra-3.0");
 
-            driver.findElement(By.xpath("//*[@id='accordion']/div/div[1]")).click();
-            movecursorm(driver);
-            Thread.sleep(1000);
-            full(driver, false, savePath, "accesssolutions-mobile-patient-hemlibra-3.0-tab1");
-
-            driver.findElement(By.xpath("//*[@id='accordion_0']/div/div[1]")).click();
-            movecursorm(driver);
-            Thread.sleep(1000);
-            full(driver, false, savePath, "accesssolutions-mobile-patient-hemlibra-3.0-tab2");
-
-            driver.findElement(By.xpath("//*[@id='accordion_295986680']/div[1]/div[1]")).click();
-            movecursorm(driver);
-            Thread.sleep(1000);
-            full(driver, false, savePath, "accesssolutions-mobile-patient-hemlibra-3.0-tab3");
-
-            driver.findElement(By.xpath("//*[@id='accordion_295986680']/div[2]/div[1]")).click();
-            movecursorm(driver);
-            Thread.sleep(1000);
-            full(driver, false, savePath, "accesssolutions-mobile-patient-hemlibra-3.0-tab4");
-
-            driver.findElement(By.xpath("//*[@id='accordion_2']/div[1]/div[1]")).click();
-            movecursorm(driver);
-            Thread.sleep(1000);
-            full(driver, false, savePath, "accesssolutions-mobile-patient-hemlibra-3.0-tab5");
-
-            driver.findElement(By.xpath("//*[@id='accordion_2']/div[2]/div[1]")).click();
-            movecursorm(driver);
-            Thread.sleep(1000);
-            full(driver, false, savePath, "accesssolutions-mobile-patient-hemlibra-3.0-tab6");
-
-            driver.findElement(By.xpath("//*[@id='accordion_2']/div[3]/div[1]")).click();
-            movecursorm(driver);
-            Thread.sleep(1000);
-            full(driver, false, savePath, "accesssolutions-mobile-patient-hemlibra-3.0-tab7");
-
-            driver.findElement(By.xpath("//*[@id='accordion_3']/div[1]/div[1]")).click();
-            movecursorm(driver);
-            Thread.sleep(1000);
-            full(driver, false, savePath, "accesssolutions-mobile-patient-hemlibra-3.0-tab8");
-
-            driver.findElement(By.xpath("//*[@id='accordion_3']/div[2]/div[1]")).click();
-            movecursorm(driver);
-            Thread.sleep(1000);
-            full(driver, false, savePath, "accesssolutions-mobile-patient-hemlibra-3.0-tab9");
-
-            driver.findElement(By.xpath("//*[@id='accordion_3']/div[3]/div[1]")).click();
-            movecursorm(driver);
-            Thread.sleep(1000);
-            full(driver, false, savePath, "accesssolutions-mobile-patient-hemlibra-3.0-tab10");
-
-            driver.findElement(By.xpath("//*[@id='accordion_4']/div[1]/div[1]")).click();
-            movecursorm(driver);
-            Thread.sleep(1000);
-            full(driver, false, savePath, "accesssolutions-mobile-patient-hemlibra-3.0-tab11");
-
-            driver.findElement(By.xpath("//*[@id='accordion_4']/div[2]/div[1]")).click();
-            movecursorm(driver);
-            Thread.sleep(1000);
-            full(driver, false, savePath, "accesssolutions-mobile-patient-hemlibra-3.0-tab12");
+            getScreenshotForAccordion(driver, "accesssolutions-mobile-patient-hemlibra-1.0", savePath, false);
 
             goToUrl(driver, "/patient/brands/hemlibra/patient-assistance-tool-page.html");
             Thread.sleep(1000);
@@ -391,28 +272,6 @@ public class Hemlibra extends SeleniumHeadless {
             driver.close();
             driver.quit();
         }
-    }
-
-    public static void movecursor(WebDriver driver) {
-
-        JavascriptExecutor jse = (JavascriptExecutor) driver;
-        Actions action = new Actions(driver);
-        WebElement element;
-
-        jse.executeScript("window.scrollTo(0, 0)");
-        element = driver.findElement(By.name("q"));
-        action.moveToElement(element).build().perform();
-    }
-
-    public static void movecursor1(WebDriver driver) {
-
-        JavascriptExecutor jse = (JavascriptExecutor) driver;
-        Actions action = new Actions(driver);
-        WebElement element;
-
-        jse.executeScript("window.scrollTo(0, 0)");
-        element = driver.findElement(By.linkText("Preguntas Frecuentes"));
-        action.moveToElement(element).build().perform();
     }
 
     public static void movecursorm(WebDriver driver) {
