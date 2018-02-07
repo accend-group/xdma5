f0613bc04c5ee69e448ff3be86273cb8e177c176
xdma5 <pgonzalez@accendgroup.com>
2018-02-07 11:18:24 -0800

    changed prod to https://www.ocrelizumabinfo.com

--- src/main/java/com/gene/screenshots/jobs/OcrelizumabinfoJob.java
+++ src/main/java/com/gene/screenshots/jobs/OcrelizumabinfoJob.java
@@ -3,19 +3,19 @@ package com.gene.screenshots.jobs;
 import com.gene.screenshots.base.ScreenshotJob;
 import com.gene.screenshots.base.annotations.Environment;
 import com.gene.screenshots.base.annotations.Job;
-import com.gene.screenshots.selenium.ocrevus.Ocrelizumbainfo;
+import com.gene.screenshots.selenium.ocrevus.Ocrelizumabinfo;
 
 @Job(name = "OcrelizumabInfo", ID = 21, info = "Automated Screenshots for the Ocrelizumb info page")
 @Environment(dev = "",
-        prod = "https://www.ocrevus.com",
+        prod = "https://www.ocrelizumabinfo.com",
         stage = "",
         local = "http://localhost:4503/content/ocrelizumabinfo/en_us",
         authordev = "https://dev-author.aem.gene.com/content/ocrelizumabinfo/en_us",
         authorstage = "https://stage-author.aem.gene.com/content/ocrelizumabinfo/en_us",
         authorprod = "https://author.aem.gene.com/content/ocrelizumabinfo/en_us",
         authorlocal= "http://localhost:4502/content/ocrelizumabinfo/en_us")
-public class OcrelizumbainfoJob extends ScreenshotJob {
-    public OcrelizumbainfoJob(){
-        setScript(new Ocrelizumbainfo());
+public class OcrelizumabinfoJob extends ScreenshotJob {
+    public OcrelizumabinfoJob(){
+        setScript(new Ocrelizumabinfo());
     }
 }
