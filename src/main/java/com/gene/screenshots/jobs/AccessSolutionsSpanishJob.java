d7c47b7fd5112ed3e1c35a81d2397e1cdb628f78
xdma5 <pgonzalez@accendgroup.com>
2018-02-19 12:20:08 -0800

    Spanish environments are up

--- src/main/java/com/gene/screenshots/jobs/AccessSolutionsSpanishJob.java
+++ src/main/java/com/gene/screenshots/jobs/AccessSolutionsSpanishJob.java
@@ -8,16 +8,13 @@ import com.gene.screenshots.base.annotations.Job;
 
 @Job(name = "Access_Solutions_ES", ID = 10, info = "Screenshot automation for Access Solutions Spanish.")
 @Environment(local = "http://localhost:4503/content/accesssolutions-site/es",
-        dev = "",
-        stage = "",
-        prod = "",
+        dev = "https://dev-genentech-access.gene.com/es",
+        stage = "https://stage-genentech-access.gene.com/es",
+        prod = "https://www.genentech-access.com/es",
         authorlocal = "http://localhost:4502/content/accesssolutions-site/es",
-        authordev = "https://dev-author.aem.gene.com/content/accesssolutions-site/es")
-
-        // not up
-        //authorprod = "https://author.aem.gene.com/content/accesssolutions-site/es",
-        //authorstage =  "https://stage-author.aem.gene.com/content/accesssolutions-site/es")
-
+        authordev = "https://dev-author.aem.gene.com/content/accesssolutions-site/es",
+        authorprod = "https://author.aem.gene.com/content/accesssolutions-site/es",
+        authorstage =  "https://stage-author.aem.gene.com/content/accesssolutions-site/es")
 public class AccessSolutionsSpanishJob extends ScreenshotJob {
 
     public AccessSolutionsSpanishJob() {
